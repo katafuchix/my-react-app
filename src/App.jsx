37e872576c5bb8fdc4cee60e6b3a63ee87e98d58
katafuchix <katafuchix@gmail.com>
2026-01-08 12:29:21 +0900

    setState sample

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,35 @@ import './App.css'
 import Layout from './components/Layout'
 import Content from './components/Content'
 
+const Button = ({ onClick, href, children }) => {
+  const style = {
+    backgroundColor: 'skyblue',
+    border: 'none',
+    padding: '8px',
+    margin: '8px',
+    borderRadius: '8px',
+    color: 'white',
+    cursor: 'pointer',
+    lineHeight: 1,
+  }
+
+  if (onClick) {
+    return (
+      <button type="button" onClick={onClick} style={style}>
+        {children}
+      </button>
+    )
+  }
+
+  if (href) {
+    return (
+      <a href={href} style={style}>
+        {children}
+      </a>
+    )
+  }
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -30,11 +59,43 @@ function App() {
     alert('クリックされました')
   }
 
+  const href = 'https://ja.react.dev/'
+
+  const [value, setValue] = useState('')
+
+  const handleChange = (e) => {
+    setValue(e.target.value)
+  }
+
+  const [isAgreed, setIsAgreed] = useState(false)
+
+  const handleAgreedChange = (e) => {
+    setIsAgreed(e.target.checked)
+  }
+
+  const handleSubmit = (e) => {
+    // フォームのデフォルト動作であるページリロードを防ぐ
+    e.preventDefault()
+    alert('利用規約に同意してフォームを送信しました！')
+  }
+
+  const [count1, setCount1] = useState(0)
+
+  const handleClick1 = () => {
+    setCount1(count1 + 1)
+  }
+
   return (
     <>
       <button type="button" onClick={handleClick}>
         クリック
       </button>
+      <Button onClick={handleClick}>ボタン</Button>
+      <Button href={href}>リンク</Button>
+      <div>
+        <input type="text" value={value} onChange={handleChange} />
+        <p>入力された値: {value}</p>
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -43,6 +104,23 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            type="checkbox"
+            checked={isAgreed}
+            onChange={handleAgreedChange}
+          />
+          利用規約に同意する
+        </label>
+        <br />
+        <button type="submit" disabled={!isAgreed}>
+          送信
+        </button>
+      </form>
+
+      <button onClick={handleClick1}>{count1}回押しました</button>
+
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
